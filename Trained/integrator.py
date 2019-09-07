b1a9b13c36a5b5c3bdcbab6ea021d0502d2e7d8a
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-09-07 15:55:13 +0530

    Functionality Optimized for Integrator

--- Trained/integrator.py
+++ Trained/integrator.py
@@ -10,34 +10,34 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 
-model = load_model('gender_detection.model')
+model = load_model('opt_model.h5')
 
 def integrator(image):
     image = cv2.imread(image)
     face, confidence = cv.detect_face(image)
     
+    l = []
     for idx, f in enumerate(face):
        
         (startX, startY) = f[0], f[1]
         (endX, endY) = f[2], f[3]
     
-        cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
-    
         face_crop = np.copy(image[startY:endY,startX:endX])
     
-        face_crop = cv2.resize(face_crop, (96,96))
+        face_crop = cv2.resize(face_crop, (128,128))
         face_crop = face_crop.astype("float") / 255.0
         face_crop = img_to_array(face_crop)
         face_crop = np.expand_dims(face_crop, axis=0)
-        # cv2.imwrite(args.image, face_crop)
     
-        tags = model.predict(face_crop)[0]
-        # print(conf)
-        # print(classes)
-    
-        # idx = np.argmax(conf)
-        # label = classes[idx]]
+        tags = model.predict(face_crop)
+        if tags[0] > 0.5:
+            case = {'Gender':'Male','Age':int(np.rint(tags[1]))}
+        else:
+            case = {'Gender':'Female','Age':int(np.rint(tags[1]))}
+
         
-        return tags
+        l.append(case)
+        
+    return l
     
 print(integrator('..//download.jpeg'))
