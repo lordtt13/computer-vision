b2ba9f9db1a31c4b4e0ebf67912b0d3626424f14
Harshanand <34165187+iamharsha1999@users.noreply.github.com>
2019-09-20 19:14:46 +0530

    Update Training.py

--- Facial Recognition/Training.py
+++ Facial Recognition/Training.py
@@ -9,9 +9,17 @@ import cv2
 import numpy as np
 import os
 from PIL import Image
+import argparse
+
+##============ Constructing the Argument Parser ==================##
+parser = argparse.ArgumentParser()
+parser.add_argument("-p","--path", required = True, help = "Path of Images and Labels")
+parser.add_argument("-x","--xmlpath", requred = True, help = "Path of the xml file for CascadeClassifier")
+args = vars(parser.parse_args())
+##================================================================##
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+detector = cv2.CascadeClassifier(args["xmlpath"])
 
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
@@ -28,6 +36,6 @@ def getImagesAndLabels(path):
     return faceSamples,ids
 
 print ("\nTraining Faces")
-faces,ids = getImagesAndLabels('Dataset')
+faces,ids = getImagesAndLabels(args["path"])
 recognizer.train(faces, np.array(ids))
 recognizer.write('trained_model.yml') 
